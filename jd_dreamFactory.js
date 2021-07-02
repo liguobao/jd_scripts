0e134c1563e1d0a66df5949f3b75e985fa8c414d
imcat <shinko168@gmail.com>
2021-07-02 12:16:13 +0800

    test

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -1059,7 +1059,7 @@ function CreateTuan() {
               if (data['ret'] === 10218 && !hasSend && (new Date().getHours() % 6 === 0)) {
                 hasSend = true;
                 $.msg($.name, '', `京喜工厂拼团瓜分电力活动团ID（activeId）已失效\n请自行抓包替换(Node环境变量为TUAN_ACTIVEID，iOS端在BoxJx)或者联系作者等待更新`);
-                if ($.isNode()) await notify.sendNotify($.name, `京喜工厂拼团瓜分电力活动团ID（activeId）已失效\n请自行抓包替换(Node环境变量为TUAN_ACTIVEID，iOS端在BoxJx)或者联系作者等待更新`)
+                //if ($.isNode()) await notify.sendNotify($.name, `京喜工厂拼团瓜分电力活动团ID（activeId）已失效\n请自行抓包替换(Node环境变量为TUAN_ACTIVEID，iOS端在BoxJx)或者联系作者等待更新`)
               }
               console.log(`开团异常：${JSON.stringify(data)}`);
             }
