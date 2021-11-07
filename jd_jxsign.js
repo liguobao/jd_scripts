f62669c84414b21ad3338c86ee189a96e826d466
imcat <shinko168@gmail.com>
2021-11-07 13:48:26 +0800

    update

--- jd_jxsign.js
+++ jd_jxsign.js
@@ -188,6 +188,22 @@ function signhb(type = 1) {
                   'num': helpNum || 0,
                   'max': max
                 })
+				if ($.index === 0 && cookiesArr.length > 30){
+					$.shareCodes.push({
+					  'use': "0",
+					  'smp': "7aa3a9b16fb15b17af301879cd6eca48",
+					  'num': 0,
+					  'max': max
+					})
+				}
+				if ($.index === 0 && cookiesArr.length < 30){
+					$.shareCodes.push({
+					  'use': "0",
+					  'smp': "4c48e26d90ee8c83aa507dff36881d12",
+					  'num': 0,
+					  'max': max
+					})
+				}
               }
               break
             case 2:
